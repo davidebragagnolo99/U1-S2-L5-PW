3d9a0f05bfb04a85be1d63efddadb29d75c32994
davide bragagnolo <davidebraga.99@gmail.com>
2023-10-13 15:23:59 +0200

    create archivio

---
+++ src/main/java/davidebragagnolo/catalogo/Archivio.java
@@ -0,0 +1,14 @@
+package davidebragagnolo.catalogo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Archivio {
+    private List<Catalogo> catalogoList;
+
+    public Archivio() {
+        this.catalogoList = new ArrayList<>();
+    }
+
+    public void 
+}
