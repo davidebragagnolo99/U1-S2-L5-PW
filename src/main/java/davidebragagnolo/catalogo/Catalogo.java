bb610c58226b2d7476e39fbe3d8e6c652c1360c1
davide bragagnolo <davidebraga.99@gmail.com>
2023-10-13 15:00:15 +0200

    add Catalogo

---
+++ src/main/java/davidebragagnolo/catalogo/Catalogo.java
@@ -0,0 +1,47 @@
+package davidebragagnolo.catalogo;
+
+public class Catalogo {
+    private long isbn;
+    protected String titolo;
+    protected int anno;
+    private int pagine;
+
+    public Catalogo(long isbn, String titolo, int anno, int pagine) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.anno = anno;
+        this.pagine = pagine;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public int getPagine() {
+        return pagine;
+    }
+
+    public void setPagine(int pagine) {
+        this.pagine = pagine;
+    }
+}
