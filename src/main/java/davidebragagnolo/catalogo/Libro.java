4223c5f803f1d1d3a9364d00fa2505dfb5b5a7f0
davide bragagnolo <davidebraga.99@gmail.com>
2023-10-13 15:05:19 +0200

    Create Libro.java

---
+++ src/main/java/davidebragagnolo/catalogo/Libro.java
@@ -0,0 +1,34 @@
+package davidebragagnolo.catalogo;
+
+public class Libro extends Catalogo {
+    private String autore;
+    private String genere;
+
+    public Libro(long isbn, String titolo, int anno, int pagine, String autore, String genere) {
+        super(isbn, titolo, anno, pagine);
+        this.autore = autore;
+        this.genere = genere;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getGenere() {
+        return genere;
+    }
+
+    public void setGenere(String genere) {
+        this.genere = genere;
+    }
+
+    @Override
+    public String toString() {
+        return "Nome libro: " + " " + titolo + ", Autore: " + autore + " " + ", Pubblicazione: " + anno + " " + ", Genere: "
+                + " " + genere;
+    }
+}
